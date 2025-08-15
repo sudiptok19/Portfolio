33fdb23d80ff67310dbc7d7d573ba4e60ecb0a22
sudipta karmakar <sudiptakarmakar490@gmail.com>
2025-08-15 16:37:59 +0530

    this

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.svg';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">Greetings Viewer</div>
+      <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo" />
+          <h1 className="email">sudiptakarmakar490@gmail.com</h1>
+      </div>
       <ul className="nav-links">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
